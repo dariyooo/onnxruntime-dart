b6a07acaa73147630cd46e9e8a8e0906469fdb9f
CaptainDario M5 <daapplab@gmail.com>
2026-08-30 23:28:01 +0200

    Remove the hand-written session layer and examples

--- packages/onnxruntime_dart/lib/onnxruntime_dart.dart
+++ packages/onnxruntime_dart/lib/onnxruntime_dart.dart
@@ -12,6 +12,5 @@ library;
 export 'src/annotations.dart' show NativeOnly, unsupportedOnWeb;
 export 'src/backend/types.dart' show OrtElementType, OrtTensorMeta;
 export 'src/bindings/config_keys.g.dart';
-export 'src/session.dart' show Session, SessionOptions;
 export 'src/ffi/runtime.dart' show OrtAbiMismatch, runtimeVersion;
 export 'src/ffi/status.dart' show OrtException;
